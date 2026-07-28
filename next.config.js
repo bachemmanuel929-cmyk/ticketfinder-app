fab9e26ea30b165dbe8af48d2e4726022a5f4d50
bachemmanuel929-cmyk <bachemmanuel929@gmail.com>
2026-07-28 17:49:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // מתעלם משגיאות טיפוסים כדי לאפשר לאתר לעלות לאוויר מיד
     ignoreBuildErrors: true,
   },
   eslint: {
-    // מתעלם משגיאות בדיקה כדי למנוע חסימות בנייה
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
